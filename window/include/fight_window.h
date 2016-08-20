9a9a11563c3f27e3b92bf293000e28931161d8f9
Seventeen017 <741253078@qq.com>
2016-08-20 16:30:21 +0800

    first commit 2016/8/20

---
+++ window/include/fight_window.h
@@ -0,0 +1,15 @@
+#ifndef FIGHT_WINDOW_H
+#define FIGHT_WINDOW_H
+#include "window/include/window.h"
+#include "data/include/place_data.h"
+#include "data/include/enemy_data.h"
+#include "data/include/hero_data.h"
+#include "data/include/fight_data.h"
+
+void Show_fight_window();
+void Show_result();
+void Show_action();
+void Show_hero_win();
+void Show_enemy_win();
+#endif // FIGHT_WINDOW_H
+
